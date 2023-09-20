3141c6988b91f14ad5f34bb81ba717d22c8f87e4
Asem Saleh <asim.saleh550@gmail.com>
2023-09-20 18:32:48 +0300

    update projects file

--- bikeshare.py
+++ bikeshare.py
@@ -4,9 +4,7 @@ import numpy as np
 import datetime as dt
 import click
 
-CITY_DATA = {'chicago': 'chicago.csv',
-             'new york city': 'new_york_city.csv',
-             'washington': 'washington.csv'}
+
 
 months = ('january', 'february', 'march', 'april', 'may', 'june')
 
